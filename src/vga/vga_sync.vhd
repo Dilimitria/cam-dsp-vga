1882ac3f80f6333fa002519e11a359cbb9a9461e
Dilimitria <dmalyga31@gmail.com>
2026-06-04 18:39:22 +0300

    vga_sync ready, not tested

--- src/vga/vga_sync.vhd
+++ src/vga/vga_sync.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity vga is
+entity vga_sync is
     generic(
         H_VISIBLE   : integer := 1280;
         H_FRONT     : integer := 110;
@@ -12,7 +12,7 @@ entity vga is
         V_VISIBLE   : integer := 720;
         V_FRONT     : integer := 5;
         V_SYNC      : integer := 5;
-        V_BACK      : integer := 20;
+        V_BACK      : integer := 20
     );
     port (
         clk         : in std_logic;
@@ -20,11 +20,54 @@ entity vga is
 
         hsync_o     : out std_logic;
         vsync_o     : out std_logic;
-        hpos_o      : out std_logic;
-        vpos_o      : out std_logic;
-        
-        active_o    : out std
-    );
-end vga;
 
-architecture 
+        hpos_o      : out std_logic_vector (11 downto 0);
+        vpos_o      : out std_logic_vector (11 downto 0);
+        
+        active_o    : out std_logic
+    );
+end vga_sync;
+
+
+
+architecture rtl_vga_sync of vga_sync is
+    constant V_TOTAL  : integer := V_VISIBLE + V_FRONT + V_SYNC + V_BACK;
+    constant H_TOTAL  : integer := H_VISIBLE + H_FRONT + H_SYNC + H_BACK;
+
+    signal h_cnt : integer range 0 to H_TOTAL-1;
+    signal v_cnt : integer range 0 to V_TOTAL-1;
+
+    
+begin
+    hsync_o <= '0' when (h_cnt >= H_VISIBLE + H_FRONT) and 
+                (h_cnt < H_VISIBLE + H_FRONT + H_SYNC) else '1';
+    vsync_o <= '0' when (v_cnt >= V_VISIBLE + V_FRONT) and 
+                (v_cnt < V_VISIBLE + V_FRONT + V_SYNC) else '1';
+
+    active_o <= '1' when (h_cnt < H_VISIBLE) and (v_cnt < V_VISIBLE) else '0';
+                        
+    hpos_o <= std_logic_vector(to_unsigned(h_cnt, 12));
+    vpos_o <= std_logic_vector(to_unsigned(v_cnt, 12));
+
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if (rst = '1') then
+                h_cnt <= 0;
+                v_cnt <= 0;
+            else 
+                if (v_cnt = V_TOTAL-1) then
+                    v_cnt <= 0;
+                else 
+                    if (h_cnt = H_TOTAL-1) then
+                        v_cnt <= v_cnt + 1;
+                        h_cnt <= 0;
+                    else 
+                        h_cnt <= h_cnt + 1;
+                    end if;
+                end if;
+            end if;
+        end if;       
+    end process;
+    
+end architecture rtl_vga_sync;
